bc944ed420d5236ff2f75e1b7fc59a555a93e569
shoelace928 <44529570+shoelace928@users.noreply.github.com>
2018-11-19 14:59:05 -0800

    11/19 Updated

--- AERO302_Winglet.m
+++ AERO302_Winglet.m
@@ -24,13 +24,65 @@ lift_NW = [mean(fz_NW_AOA_0) mean(fz_NW_AOA_4) mean(fz_NW_AOA_8) mean(fz_NW_AOA_
 %Lift - No Winglet
 rho = 1.225 ;   %standard atmospheric density
 S = .155575 ;   %wing area
-c_l = (2.*lift_NW)./((rho*(v^2)*S)) ;   %coefficient of lift 
+c_l_NW = (2.*lift_NW)./((rho*(v^2)*S)) ;   %coefficient of lift 
 
 figure(1)
-plot(alpha,c_l) 
+plot(alpha,c_l_NW) 
 xlabel('alpha (degrees)')
 ylabel('Coefficient Lift')
-title('C_l versus Angle of Attack')
+title('C_l versus Angle of Attack -- Without Winglet')
+
+%Drag - No Winglet
+load('WOff.mat')
+NW_AOA_0 = WOff{1} ;
+fy_NW_AOA_0 = NW_AOA_0(:,2) ;
+NW_AOA_4 = WOff{2} ;
+fy_NW_AOA_4 = NW_AOA_4(:,2) ;
+NW_AOA_8 = WOff{3} ;
+fy_NW_AOA_8 = NW_AOA_8(:,2) ;
+NW_AOA_9 = WOff{4} ;
+fy_NW_AOA_9 = NW_AOA_9(:,2) ;
+NW_AOA_10 = WOff{5} ;
+fy_NW_AOA_10 = NW_AOA_10(:,2) ;
+NW_AOA_11 = WOff{6} ;
+fy_NW_AOA_11 = NW_AOA_11(:,2) ;
+NW_AOA_12 = WOff{7} ;
+fy_NW_AOA_12 = NW_AOA_12(:,2) ;
+NW_AOA_13 = WOff{8} ;
+fy_NW_AOA_13 = NW_AOA_13(:,2) ;
+NW_AOA_14 = WOff{9} ;
+fy_NW_AOA_14 = NW_AOA_14(:,2) ;
+
+for ii = 1:9
+    R(ii) = ((6.4136e-9)*alpha(ii)^7) - ((2.4054e-7)*alpha(ii)^6) + ((6.3238e-7)*alpha(ii)^5) + ((6.7126e-5)*alpha(ii)^4)- ((8.0391e-4)*alpha(ii)^3) + ((.0023*alpha(ii)^2)) + ((.0021*alpha(ii))) + .0453 ;
+    drag_nosting = [mean(fy_NW_AOA_0) mean(fy_NW_AOA_4) mean(fy_NW_AOA_8) mean(fy_NW_AOA_9) mean(fy_NW_AOA_10) mean(fy_NW_AOA_11) mean(fy_NW_AOA_12) mean(fy_NW_AOA_13) mean(fy_NW_AOA_14)] ;
+    drag_sting = drag_nosting - 1.22 ; %drag force accounting for sting
+    drag_NW(ii) = R(ii) * drag_sting(ii) ;
+end 
+c_d_nosting = (2.*drag_nosting)./((rho*(v^2)*S)) ;   %uncorrected coefficient of drag
+c_d_NW = (2.*drag_NW)./((rho*(v^2)*S)) ;    %coefficient of drag
+
+figure(2)
+plot(alpha,c_d_NW,alpha,c_d_nosting)
+xlabel('alpha (degrees)')
+ylabel('coefficient of drag')
+title('C_d versus Angle of Attack -- Without Winglet')
+legend('corrected for sting','raw data')
+axis([-5 15 0 .4])
+
+ratio_NW = lift_NW./drag_nosting ;
+ratio_NW_corrected = lift_NW./drag_NW ;    %L/D efficiency ratio
+figure(3)
+plot(alpha,ratio_NW_corrected,alpha,ratio_NW) 
+xlabel('alpha (degrees)')
+ylabel('L/D efficiency ratio')
+title('L/D versus Angle of Attack')
+legend('corrected for sting','raw values')
+axis([-5 15 0 55])
+
+
+
+
 
 
 
