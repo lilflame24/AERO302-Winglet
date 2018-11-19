7ed5ec0562b90b4394b243cf48190688bc0b4d41
shoelace928 <44529570+shoelace928@users.noreply.github.com>
2018-11-18 20:27:20 -0800

    11/18 Updated: only uploaded data for NW

---
+++ AERO302_Winglet.m
@@ -0,0 +1,7 @@
+%% AERO302 Winglet Code
+    %import data from excel to workspace, there are differing sample sizes
+    %so to stay consistent lets use 1:70 
+clear all; close all; clc; 
+
+alpha = [0 4 8 9 10 11 12 13 14] ;  %measured angles of attack
+
